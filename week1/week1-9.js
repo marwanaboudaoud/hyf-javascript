b1c34597c5a656e13023bff73b9afc35e6d7fb83
marwanaboudaoud <marwanaboudoud@gmail.com>
2018-07-05 13:51:55 +0200

    what % does

---
+++ week1/week1-9.js
@@ -0,0 +1,16 @@
+var x = 7;
+x = x % 3;
+console.log("the new value of x variable would be: 1");
+
+
+var e = 5;
+var u = 2;
+var s = e % u;
+console.log("the value of s variable would be:1")
+
+/*5 % 2 = 1 because
+Math.floor(5 / 2) = 2 (Quotient)
+2 * 2 = 4
+5 - 4 = 1 (Remainder) */
+
+
