9f67d4a67d41f8080a3800188206ffb2618b941d
marwanaboudaoud <marwanaboudoud@gmail.com>
2018-07-05 13:48:35 +0200

    Array

---
+++ week1/week1-6.js
@@ -0,0 +1,8 @@
+var nothingArray = [];
+console.log("no any value in nothingArray");
+console.log(nothingArray);
+var bestAnimals = ["cat" , "hamester" , "birds" , "rabbit"];
+console.log(bestAnimals);
+bestAnimals.push("baby big");
+console.log(bestAnimals);
+
