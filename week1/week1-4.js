67483bddcf1b93859acfbf6b5ac12f3df1f91319
marwanaboudaoud <marwanaboudoud@gmail.com>
2018-07-05 13:46:13 +0200

    declare variable y

---
+++ week1/week1-4.js
@@ -0,0 +1,6 @@
+var y = "marwan";
+console.log('the value of y will be: marwan');
+console.log(y);
+y = "my name is marwan";
+console.log('the value of y now will be:my name is marwan');
+console.log(y);
