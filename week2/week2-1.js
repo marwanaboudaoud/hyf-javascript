8bb05f92b08b9711762d36ac75517e3e049be41c
marwanaboudaoud <marwanaboudoud@gmail.com>
2018-07-12 19:23:39 +0200

    Strings!

---
+++ week2/week2-1.js
@@ -0,0 +1,6 @@
+let myString = "hello,this,is,a,difficult,to,read,sentence";
+console.log(myString);
+console.log(`the length is ${myString.length}`);
+a = myString.replace(/,/g, " ");
+console.log(a);
+
